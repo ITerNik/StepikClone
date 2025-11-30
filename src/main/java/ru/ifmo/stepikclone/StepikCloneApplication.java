827b7e8cf7dff448debd68be5010252b376d0c9c
ITerNik <ternik2705@list.ru>
2025-11-30 18:10:53 +0300

    Stepik-3 Integrated with YouTrack

---
+++ src/main/java/ru/ifmo/stepikclone/StepikCloneApplication.java
@@ -0,0 +1,13 @@
+package ru.ifmo.stepikclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepikCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepikCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ifmo/stepikclone/StepikCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifmo.stepikclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepikCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
